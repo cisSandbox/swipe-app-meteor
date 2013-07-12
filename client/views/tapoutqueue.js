5d4bb55e20bd20e0d1934c43514f630a60725821
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-07-11 22:57:25 -0400

    finished the tapout queue

--- client/views/tapoutqueue.js
+++ client/views/tapoutqueue.js
@@ -5,5 +5,21 @@
  */
 
 Template.tapoutqueue.signedInStudents = function() {
-    return Visits.find({timeOut: {$not: 'null'}});
+    return Visits.find({'timeOut': null});
 };
+
+Template.student.timeDiff = function() {
+	var today = new Date();
+	var diff = Math.ceil((today.getTime() - this.timeIn) / 216000);
+	return diff + ' minutes';
+};
+
+setInterval(function() {
+	location.reload();
+}, 10000);
+
+Template.student.events({
+	'click button': function() {
+		Visits.update({_id: this._id}, {$set: {'timeOut': new Date()}});
+	}
+});
