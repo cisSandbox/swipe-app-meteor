03f3ac750aefc1fff9f0aa4a9eb9382a284aaaf5
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-07-18 16:44:36 -0400

    preliminary user update support

--- client/views/dashboard.js
+++ client/views/dashboard.js
@@ -6,6 +6,8 @@
 
  */
 
+// General dashboard template stuff
+
 Template.dashboard.currentUser = function() {
     return Meteor.user();
 };
@@ -22,16 +24,19 @@ Template.dashboard.events({
     }
 });
 
+// Tutor dashboard stuff
 
 Template.tutordashboard.currentlyTutoring = function() {
     return WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null});
 };
 
+Template.edituser.courses = function() {
+    return Courses.find();
+};
 
 Template.tutordashboard.events({
     'click #tutorButton': function() {
-        var visit = WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null});
-        if (visit) {
+        if (WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null})) {
             WorkVisits.update({_id: visit._id}, {$set: {timeOut: new Date()}});
         } else {
             WorkVisits.insert({tutorId: Meteor.userId(), timeIn: new Date(), timeOut: null});
@@ -39,33 +44,44 @@ Template.tutordashboard.events({
     }
 });
 
+Template.edituser.events({
+    'click #editUserSubmit': function(e, t) {
+        e.preventDefault();
+        var name    = t.find('#editUserName').value,
+            courses = $(t.find('#editCourses')).val();
+        console.log(courses);
+        Meteor.users.update({_id: Meteor.userId()}, {$set: {'profile.name': name, 'profile.canTutor': courses}});
+        alert('Profile Updated');
+    }
+});
+
+// Admin dashboard stuff
+
 Template.admindashboard.users = function() {
     return Meteor.users.find();
 };
 
 Template.userstable.events({
     'click #deleteUser': function() {
-        var confirm = window.confirm('Are you sure? This cannot be undone.');
-        if (confirm) {
+        if (window.confirm('Are you sure? This cannot be undone.')) {
             Meteor.call('adminRemoveUser', this._id);
         }
     }
 });
-
 
 Template.createuser.roles = function() {
     return Meteor.roles.find();
 };
 
 Template.createuser.events({
-    'click #inputSubmit': function(e, t) {
+    'click #createUserSubmit': function(e, t) {
         e.preventDefault();
         console.log('click');
-        var email     = t.find('#inputEmail').value,
-            name      = t.find('#inputName').value,
-            shortname = t.find('#inputShortname').value,
-            password  = t.find('#inputPassword').value,
-            role      = t.find('#inputRole').value;
+        var email     = t.find('#createUserEmail').value,
+            name      = t.find('#createUserName').value,
+            shortname = t.find('#createUserShortname').value,
+            password  = t.find('#createUserPassword').value,
+            role      = t.find('#createUserRole').value;
         Meteor.call('adminCreateUser', {email: email, password: password, profile: {name: name, shortname: shortname}}, role);
     }
 });
