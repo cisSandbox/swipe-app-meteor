b7ca64b346671282a718ca11783ea36acfeb72cc
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-09-05 10:05:19 -0400

    started router rewrite

--- client/router.js
+++ client/router.js
@@ -15,40 +15,69 @@ Meteor.Router.add({
 
 var tutorPages       = ['dashboard', 'tutorqueue', 'tapoutqueue', 'home', 'tutorform', 'dashboard'];
 var professorPages   = ['dashboard', 'home', 'tapoutqueue', 'dashboard'];
-var frontScreenPages = ['swipescreen', 'tutorqueue', 'tapoutqueue', 'home'];
+var frontScreenPages = ['swipescreen', 'tapoutqueue', 'home'];
 
+
+// Meteor.Router.filters({
+//     'checkLogin': function(page) {
+//         var goto;
+//         if (Meteor.user()) {
+//             if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
+//                 goto = page;
+//             } else if (Roles.userIsInRole(Meteor.userId(), 'frontScreen') && _.contains(frontScreenPages, page)) {
+//                 goto = page;
+//             } else if (Roles.userIsInRole(Meteor.userId(), 'professor') && _.contains(professorPages, page)) {
+//                 goto = page;
+//             } else if (Roles.userIsInRole(Meteor.userId(), 'tutor') && _.contains(tutorPages, page)) {
+//                 // This is realllllly convoluted. I should fix it.
+//                 var v = !WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null});
+//                 if (page === 'tutorform' && v) {
+//                     Meteor.Messages.postMessage('error', 'Tutors must be currently working to access the tutor form page.');
+//                     goto = 'login';
+//                 } else {
+//                     goto = page;
+//                 }
+//             } else if (page === 'not_found') {
+//                 goto = page;
+//             } else {
+//                 goto = 'login';
+//                 Meteor.Messages.postMessage('error', 'You do not have permission to access the ' + page + 'page.');
+//             }
+//         } else {
+//             goto = 'login';
+//             Meteor.Messages.postMessage('error', 'You must be logged in to access the ' + page + 'page.');
+//         }
+//         return goto;
+//     }
+// });
 
 Meteor.Router.filters({
     'checkLogin': function(page) {
-        var goto;
-        if (Meteor.user()) {
-            if (Roles.userIsInRole(Meteor.userId(), 'admin')) {
-                goto = page;
-            } else if (Roles.userIsInRole(Meteor.userId(), 'frontScreen') && _.contains(frontScreenPages, page)) {
-                goto = page;
-            } else if (Roles.userIsInRole(Meteor.userId(), 'professor') && _.contains(professorPages, page)) {
-                goto = page;
-            } else if (Roles.userIsInRole(Meteor.userId(), 'tutor') && _.contains(tutorPages, page)) {
-                // This is realllllly convoluted. I should fix it.
-                var v = !WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null});
-                if (page === 'tutorform' && v) {
-                    Meteor.Messages.postMessage('error', 'Tutors must be currently working to access the tutor form page.');
-                    goto = 'login';
-                } else {
-                    goto = page;
-                }
-            } else if (page === 'not_found') {
-                goto = page;
-            } else {
-                goto = 'login';
-                Meteor.Messages.postMessage('error', 'You do not have permission to access the ' + page + 'page.');
-            }
+        if(Meteor.user()) {
+            return page;
         } else {
-            goto = 'login';
             Meteor.Messages.postMessage('error', 'You must be logged in to access the ' + page + 'page.');
+            return 'login';
         }
-        return goto;
+    },
+    'checkFrontScreen': function(page) {
+        if(Roles.userIsInRole(Meteor.userId(), 'admin') || Roles.userIsInRole(Meteor.userId(), 'frontScreen')) {
+            return page;
+        } else {
+            Meteor.Messages.postMessage('error', 'You must have admin permissions to access the ' + page + ' page.');
+            return 'login';
+        }
+    },
+    'checkWorkingTutor': function(page) {
+        if(Roles.userIsInRole(Meteor.userId(), 'admin') || WorkVisits.findOne({tutorId: Meteor.userId(), timeOut: null})) {
+            return page;
+        } else {
+            Meteor.Messages.postMessage('error', 'You must be currently working in order to fill out a tutor form.');
+            return 'login';
+        }
     }
 });
 
-Meteor.Router.filter('checkLogin', {except: 'login'});
+Meteor.Router.filter('checkLogin', {except: [ 'not_found', 'login' ]});
+Meteor.Router.filter('checkFrontScreen', {only: frontScreenPages});
+Meteor.Router.filter('checkWorkingTutor', {only: 'tutor-form'});
