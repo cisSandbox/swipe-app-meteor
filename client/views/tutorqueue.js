5452a79f036a6740a0b9a29db888e6f0f5be07a0
Nick Hentschel <hentschel.nicholas@gmail.com>
2013-09-05 10:26:34 -0400

    added back to dashboard button to tutorqueue page

--- client/views/tutorqueue.js
+++ client/views/tutorqueue.js
@@ -15,8 +15,19 @@ Template.tutorqueue.needHelpStudents = function() {
     return Visits.find({'timeOut': null, 'needHelp': true});
 };
 
+Template.tutorqueue.checkFrontScreen = function() {
+    return !Roles.userIsInRole(Meteor.userId(), 'frontScreen');
+};
+
+Template.tutorqueue.events({
+    'click .back-to-dash': function() {
+        Meteor.Router.to('dashboard');
+    }
+});
+
 Template.needhelp.events({
     'click button': function() {
         Meteor.Router.to('/tutor-form/' + this._id);
     }
 });
+
